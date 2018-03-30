3c1c4e6be7bbf5abf44b024f8002b067c8445406
Jack Wu <jackwu350@yahoo.com>
2018-03-29 22:56:17 -0700

    refactored initial app

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,89 +1,25 @@
-//
-//
-// window.onload = function(){
-//
-//
-//   var button = document.getElementById('button');
-//   button.addEventListener('click', () => {
-//
-//     var list = document.getElementById('list');
-//
-//     var list_item = document.createElement('li');
-//     list_item.innerHTML = document.getElementById('input').value;
-//
-//     list.appendChild(list_item);
-//
-//   });
-//
-//
-// }
+function App(){
 
-window.onload = () => {
-  let app = document.querySelector('main')
-  let h1 = document.createElement('h1')
+  var main = document.querySelector('main');
+  var h1 = document.createElement('h1');
+  h1.textContent = "Simple Todo list";
+  main.append(h1);
 
-  h1.textContent = 'Simple Todo List'
-
-  let ul = document.createElement('ul');
-
-
-
-  let input = document.createElement('input')
-  input.placeholder = "Enter Todo Item";
-  input.type = "text"
-
-  let array = [];
-  input.addEventListener('keyup', (e) => {
-
-
-    if(e.keyCode === 13 ){
-      let li = "<li>" + e.target.value + "<button id='delete' onclick='clearItem()'> Delete </button> </li>";
-      ul.innerHTML += li;
-      array.push(li);
-
-      window.localStorage.setItem("items", JSON.stringify(array));
-
-      //Reset input field
-      input.value = ""
-    } else{
-
-      return;
-    }
-
-  });
-
-
-  app.appendChild(h1);
-  app.appendChild(ul);
-  app.appendChild(input);
-}
-
-function clearItem(){
-
-  event.currentTarget.parentElement.remove();
-
-  let items = JSON.parse(window.localStorage.getItem("items"));
-
-  let index = 0;
-
-
-  for( var i in items){
-      console.log(event.currentTarget.parentElement, i, items[i]);
-      if(JSON.stringify(event.currentTarget.parentElement) === items[i]){
-
-        return;
-
-      }
-
-
+  var createInput = function(){
+    console.log("hello world")
   }
 
-  items.splice(i,1);
-  // console.log(items);
-
-  window.localStorage.setItem("items", JSON.stringify(items));
-
+  return {
+    createInput
+  }
 
 
 
+}
+
+
+
+window.onload = () => {
+  var app = new App();
+  app.createInput();
 }

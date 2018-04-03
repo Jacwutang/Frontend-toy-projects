df6c1087a8936a71d484e7448bc33077cb8729bd
Jack Wu <jackwu350@yahoo.com>
2018-04-03 00:12:40 -0700

    refactored todo-list into functions

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,17 +1,88 @@
-function App(){
 
+
+
+function App(){
+  //create html structure + starter DOM elements
   var main = document.querySelector('main');
   var h1 = document.createElement('h1');
   h1.textContent = "Simple Todo list";
-  main.append(h1);
 
-  var createInput = function(){
-    console.log("hello world")
+  document.body.prepend(h1);
+
+  let input = document.createElement('input');
+  input.placeholder = "Enter todo item";
+
+  let button = document.createElement('button');
+  button.textContent = "Add";
+  button.onclick = appendToList;
+
+  let ul = document.createElement('ul');
+  var listElements = [];
+
+  main.prepend(ul);
+  main.append(input);
+  main.append(button);
+
+
+  function appendToList(){
+      //create li and append to list
+      let delButton = document.createElement('button');
+      delButton.textContent = "Delete";
+      delButton.onclick = deleteFromList;
+
+      let li = document.createElement('li');
+      li.textContent = document.querySelector('input').value;
+      li.append(delButton);
+      ul.append(li);
+
+      //add to localStorage
+
+      addToStorage(li);
+      let list = JSON.parse(localStorage.getItem('todolist'));
+      //console.log(li) returns array of empty objects
+
+
+      //clear input
+      input.value = "";
+
+
   }
 
-  return {
-    createInput
+  function addToStorage(li){
+    listElements.push(li);
+
+    localStorage.setItem('todolist', JSON.stringify(listElements));
+
   }
+
+  function removeFromStorage(){
+
+  }
+
+  function deleteFromList(){
+    //remove li from list
+    let li = this.parentElement;
+    li.remove();
+
+
+    //remove from localStorage
+    removeFromStorage();
+  }
+
+  var fetchTodos = function(){
+    // return Todos from localStorage
+  }
+
+
+  return{
+    fetchTodos
+  }
+
+
+
+
+
+
 
 
 
@@ -20,6 +91,6 @@ function App(){
 
 
 window.onload = () => {
-  var app = new App();
-  app.createInput();
+  var app = App();
+
 }
